85e87fe089790f0e03ac170af48e403f9cb17702
pooja <pooja.kumari@nprservices.in>
2025-11-11 20:49:21 +0530

    chnages

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -25,9 +25,9 @@ import '@/app/globals.css'
 import '@assets/iconify-icons/generated-icons.css'
 
 export const metadata = {
-  title: 'Vuexy - MUI Next.js Admin Dashboard Template',
+  title: 'ZOBOX ख़ुशियों वाला BOX',
   description:
-    'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
+    'Zobox - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
 }
 
 const RootLayout = async props => {
